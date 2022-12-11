3630f54159f5311a4d973c49a328f1de55a0aab1
ismayilras@code.edu.az <ismayilras@code.edu.az>
2022-12-11 09:58:37 +0400

    Second

---
+++ HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeWork2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a ededini daxil edin");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b ededini daxil edin");
+            int b=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qaliq:"+ (a%b));
+        }
+    }
+}
